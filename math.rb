30a22299b424d02a340f9c5f79bb570888880840
Justin Veirs <justinveirs@gmail.com>
2014-11-05 17:48:40 -0500

    adding in comments so I understand better

--- math.rb
+++ math.rb
@@ -9,27 +9,27 @@
 # >= greater-than-equal
 
 puts "I will now count my chickens. Holler"
-
+# maching is outputting I will now count my chickens
 puts "Hens #{25 + 30 / 6}"
-
+# honestly, I don't think this is acutally adding 25 to 30 then dividing by 6; no way.
 puts "Roosters #{100 - 25 * 3 % 4}"
-
+# similar to the above line of code.
 puts "Now I will count the eggs:"
-
+# prints to the screen Now I will count the eggs
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
+# this adds, subtracts, multiplies, and %, which I think is the module.
 puts "Is it true that 3 + 2 < 5 - 7?"
-
+# printing to the screen is it true that 3 plus 2 is less than 5 minus 7.
 puts 3 + 2 < 5 - 7
-
+# false
 puts "what is 3 + 2? #{3 + 2}"
-
+# prints to the screen 3 plus 2
 puts "what is 5 - 7? #{5 - 7}"
-
+# prints to the screen 5 - 7....you know the rest.basic math
 puts "Oh, hey, that's why it's false."
-
+# prints Oh, hey..
 puts "How about some more, eeeyy :)"
-
+# prints to the terminal or running the program..How about some more..
 puts "Is it greater? #{5 > -2}"
 
 puts "Is it greater or equal? #{5 >= -2}"
