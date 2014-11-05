de58a728d5f3831a40fbf41ca627df84cc045253
Justin Veirs <justinveirs@gmail.com>
2014-11-05 15:19:28 -0500

    just a refresh

---
+++ ex1.rb
@@ -0,0 +1,5 @@
+puts "Yo Yo, remember me?"
+puts "It's white people wednesday"
+puts "String Cheese are playing this evening, but I'm not going"
+puts "I will be a ruby and rails master"
+puts "Metallica has a song called master"
