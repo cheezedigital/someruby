b7b51090cc1071f14155bfc7fb2fcbeeb79630a1
Justin Veirs <justinveirs@gmail.com>
2014-11-06 11:16:44 -0500

    writing simple

---
+++ about.rb
@@ -0,0 +1,15 @@
+my_name = "Justin Veirs"
+my_age = 34
+my_height = 71
+my_weight = 225
+my_eyes = "Hazel"
+my_teeth = "White"
+my_hair = "Black?"
+
+puts "Lets talk about #{my_name}!"
+puts "He's #{my_height} inches tall!"
+puts "He's #{my_weight} pounds heavy"
+puts "Actually, I'm trying to lose some weight, and get into shape. It feels great!"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "If I add #{my_age}, my #{my_height}, my #{my_weight}, I get #{my_age + my_height + my_weight}."
