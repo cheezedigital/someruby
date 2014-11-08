4e909786c47f9a2f21f0275e7300f070c542af21
Justin Veirs <justinveirs@gmail.com>
2014-11-08 17:48:19 -0500

    more prints

---
+++ more_printing.rb
@@ -0,0 +1,21 @@
+puts "justin will eventually get this"
+puts "it's starting to get fucking cold outside"
+puts "the heat isn't even on, and I hope I don't get let go at the new job"
+puts "primus is playing this evening. such a good show, really, the funguy"
+puts "." * 10  # what's it doing?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+# print end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11 + end12
