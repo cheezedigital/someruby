b3a5aef6ad1ae91488c769a2d38c11d0e6cb4647
Justin Veirs <justinveirs@gmail.com>
2014-11-09 17:00:32 -0500

    intro to new line character

---
+++ what_was_that.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+black_slash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts black_slash_cat
+puts fat_cat
