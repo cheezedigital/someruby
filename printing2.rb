d9d13368a582c3a164478d91e818037b1bd41792
Justin Veirs <justinveirs@gmail.com>
2014-11-09 16:29:07 -0500

    more puts and printing to the screen

---
+++ printing2.rb
@@ -0,0 +1,14 @@
+days = "Mon Tues Wed Thur Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug\nSept\nOct\nNov\nDec"
+
+puts "here are the days: #{days}"
+puts "here are the months: #{months}"
+
+puts """
+there's something going on here,
+and I'm not sure what exactly,
+but life as we know it is,
+extremely complicated, really,
+and so, on that note, please,
+let's smoke about it.
+"""
