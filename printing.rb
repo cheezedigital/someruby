a785093d0f330d90511574c1ca502fc7c5c25744
Justin Veirs <justinveirs@gmail.com>
2014-11-09 16:01:43 -0500

    adding another print page, and submitting another lesson

--- printing.rb
+++ printing.rb
@@ -2,3 +2,12 @@ formatter = "%{first} %{second} %{third} %{fourth}"
 
 puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
 puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you could give up.",
+  third: "But it didn't sing.",
+  fourth: "So I smoked a blunt."
+}
