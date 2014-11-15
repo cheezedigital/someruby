b26a3d85bcdea14ccb7e7862b73c4f790c7728ce
Justin Veirs <justinveirs@gmail.com>
2014-11-15 16:21:26 -0500

    working with variables

---
+++ exercise_13.rb
@@ -0,0 +1,10 @@
+first, second, third = ARGV
+
+puts "your first variable is: #{first}"
+puts "your second variable is: #{second}"
+puts "your third variable is: #{third}"
+
+
+#when running th program through the terminal, we can pass in the variable
+#so ruby exercise_13.rb first, 2nd, 3rd.
+#really, though. anything can be passed in. holler. yo. joe. etc..
