16f4d42a8cd57efce5186edf2e6f3edbab15b2e4
Justin Veirs <justinveirs@gmail.com>
2014-12-08 16:37:02 -0500

    trying to get another file to open

---
+++ exercise_16.rb
@@ -0,0 +1,32 @@
+filename = ARGV.first
+
+puts "we're going erase #{filename}."
+puts "if you don't want that, hit CTRL-C (^C)."
+puts "if you do want that, hit RETURN."
+
+$stdin.gets
+
+puts "Opening the file..."
+target = open(filename, 'w')
+
+puts "truncating the file..Goodbye."
+target.truncate(0)
+
+puts "Now I'm going to ask you for three lines"
+
+print "line 1: "
+line1 = $stdin.gets.chomp
+print "line 2: "
+line2 = $stdin.gets.chomp
+print "line 3: "
+line3 = $stdin.gets.chomp
+
+puts "I'm going to write these to the file"
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+
+puts "and finally, we close it"
+target.close
